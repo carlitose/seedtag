91d0c91443ac5437447251c2a171fbd77b5f71df
Carlo Giuseppe Sergi <carlo.sergi@klarna.com>
2023-01-14 19:42:33 +0100

    validator avj

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,13 +1,13 @@
 import request from 'supertest';
 import app from './app'; // importare il file app.ts in cui è definito il server
 
-describe('Test endpoint POST', () => {
-  it('Deve rispondere con un messaggio di successo per una richiesta JSON valida', async () => {
+describe('Test radar POST', () => {
+  it('should response with an error if the body is not a YVH request', async () => {
     const res = await request(app)
-      .post('/endpoint')
-      .send({ message: 'ciao' });
+      .post('/radar')
+      .send({ message: 'EMPIRE IS REBORN!' });
 
-    expect(res.status).toBe(200);
+    expect(res.status).toBe(400);
     expect(res.text).toBe('JSON ricevuto');
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
-import express  from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
+import Ajv from 'ajv';
+import { schema } from './validator'
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/endpoint', (req: express.Request, res: express.Response) => {
+const ajv = new Ajv();
+const validate = ajv.compile(schema)
+
+app.post('/radar', (req: express.Request, res: express.Response) => {
     const jsonBody = req.body;
+    console.log('dio fa')
+    if (!validate(jsonBody)) {
+        console.log('sono qui')
+        return res.status(400).json({ errors: validate.errors });
+    }
     console.log(jsonBody);
     res.send('JSON ricevuto');
 });
