85834313f9645f9bc3a34ac22f53602fa1ccf658
Carlo Giuseppe Sergi <carlo.sergi@klarna.com>
2023-01-14 21:36:49 +0100

    adding validator and tests of it

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,12 +2,18 @@ import request from 'supertest';
 import app from './app'; // importare il file app.ts in cui è definito il server
 
 describe('Test radar POST', () => {
-  it('should response with an error if the body is not a YVH request', async () => {
+  it('should be response with a 200', async()=>{
+    const res = await request(app)
+      .post('/radar')
+      .send( {"protocols":["avoid-mech"],"scan":[{"coordinates":{"x":0,"y":40},"enemies":{"type":"soldier","number":10}}]});
+
+    expect(res.status).toBe(200);
+  })
+  it('should response with status 400 if the body is not a YVH request', async () => {
     const res = await request(app)
       .post('/radar')
       .send({ message: 'EMPIRE IS REBORN!' });
 
     expect(res.status).toBe(400);
-    expect(res.text).toBe('JSON ricevuto');
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,25 @@
-import express from 'express';
+import express, {Response, Request, NextFunction} from 'express';
 import bodyParser from 'body-parser';
-import Ajv from 'ajv';
-import { schema } from './validator'
+import { celebrate, errors, isCelebrateError } from 'celebrate';
+
+import { yvhRequestSchema } from './structure/schema'
 
 const app = express();
 app.use(bodyParser.json());
 
-const ajv = new Ajv();
-const validate = ajv.compile(schema)
 
-app.post('/radar', (req: express.Request, res: express.Response) => {
+app.post('/radar', celebrate({
+    body: yvhRequestSchema
+}), 
+async (req: Request, res: Response) => {
     const jsonBody = req.body;
-    console.log('dio fa')
-    if (!validate(jsonBody)) {
-        console.log('sono qui')
-        return res.status(400).json({ errors: validate.errors });
-    }
-    console.log(jsonBody);
     res.send('JSON ricevuto');
 });
 
-app.listen(3000, () => {
-    console.log('Server in esecuzione sulla porta 3000');
-});
+app.use(errors({statusCode:400}));
 
+app.listen(3000, () => {
+    console.log('The force is powerful on the port 3000');
+});
+ 
 export default app
